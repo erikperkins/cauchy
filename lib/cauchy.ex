432e003d0e9f178d4dfab44ff1261d5f3a76abe9
Erik Perkins <erik.andrew.perkins@gmail.com>
2023-03-17 00:28:39 -0700

    Change main web application to submodule pascal

--- lib/cauchy.ex
+++
@@ -1,9 +0,0 @@
-defmodule Cauchy do
-  @moduledoc """
-  Cauchy keeps the contexts that define your domain
-  and business logic.
-
-  Contexts are also responsible for managing your data, regardless
-  if it comes from the database, an external API or others.
-  """
-end
